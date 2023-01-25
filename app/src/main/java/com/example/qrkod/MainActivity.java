b7fdeab1c3354dcdd9dd41565010579e0d800809
Elcsa <81393593+Elcsa@users.noreply.github.com>
2023-01-25 11:24:57 +0100

    gombok

--- app/src/main/java/com/example/qrkod/MainActivity.java
+++ app/src/main/java/com/example/qrkod/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.qrkod;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 public class MainActivity extends AppCompatActivity {
-
+    private Button buttonscan;
+    private TextView textViewQrresult;
+    private ImageView imageViewQrresult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
     }
 }
